a1732d13310224b9c6405674e34d24431d574038
Basuke Suzuki <basuke@mac.com>
2023-08-06 22:45:09 -0700

    Initial Commit

---
+++ Cube/CubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeApp.swift
+//  Cube
+//
+//  Created by Basuke Suzuki on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
