cb458b7fbd96702dbc203b58f8569c7d89f29c0b
Basuke Suzuki <basuke@mac.com>
2023-08-10 16:12:56 -0700

    Complete set of movement

--- Cube/Move.swift
+++ Cube/Move.swift
@@ -16,23 +16,32 @@ enum PrimitiveMove: Character, CaseIterable {
     case B = "B"
     case R = "R"
     case L = "L"
+    case Uw = "u"
+    case Dw = "d"
+    case Fw = "f"
+    case Bw = "b"
+    case Rw = "r"
+    case Lw = "l"
     case x = "x"
     case y = "y"
     case z = "z"
+    case M = "M"
+    case E = "E"
+    case S = "S"
 
     var axis: Vector {
         switch self {
-        case .R, .x:
+        case .R, .Rw, .x:
             Axis.X
-        case .L:
+        case .L, .Lw, .M:
             Axis.X.negative
-        case .U, .y:
+        case .U, .Uw, .y:
             Axis.Y
-        case .D:
+        case .D, .Dw, .E:
             Axis.Y.negative
-        case .F, .z:
+        case .F, .Fw, .z, .S:
             Axis.Z
-        case .B:
+        case .B, .Bw:
             Axis.Z.negative
         }
     }
@@ -45,9 +54,18 @@ enum PrimitiveMove: Character, CaseIterable {
         case .D: return { $0.position.y < 0 }
         case .F: return { $0.position.z > 0 }
         case .B: return { $0.position.z < 0 }
+        case .Rw: return { $0.position.x >= 0 }
+        case .Lw: return { $0.position.x <= 0 }
+        case .Uw: return { $0.position.y >= 0 }
+        case .Dw: return { $0.position.y <= 0 }
+        case .Fw: return { $0.position.z >= 0 }
+        case .Bw: return { $0.position.z <= 0 }
         case .x: return { _ in true }
         case .y: return { _ in true }
         case .z: return { _ in true }
+        case .M: return { $0.position.x == 0 }
+        case .E: return { $0.position.y == 0 }
+        case .S: return { $0.position.z == 0 }
         }
     }
 }
@@ -109,6 +127,52 @@ let allMoves: [String:Move] = [
     "R2": Move(.R, twice: true),
     "L2": Move(.L, twice: true),
 
+    // wide moves
+
+    "Uw": Move(.Uw),
+    "Dw": Move(.Dw),
+    "Fw": Move(.Fw),
+    "Bw": Move(.Bw),
+    "Rw": Move(.Rw),
+    "Lw": Move(.Lw),
+
+    "Uw'": Move(.Uw, prime: true),
+    "Dw'": Move(.Dw, prime: true),
+    "Fw'": Move(.Fw, prime: true),
+    "Bw'": Move(.Bw, prime: true),
+    "Rw'": Move(.Rw, prime: true),
+    "Lw'": Move(.Lw, prime: true),
+
+    "Uw2": Move(.Uw, twice: true),
+    "Dw2": Move(.Dw, twice: true),
+    "Fw2": Move(.Fw, twice: true),
+    "Bw2": Move(.Bw, twice: true),
+    "Rw2": Move(.Rw, twice: true),
+    "Lw2": Move(.Lw, twice: true),
+
+    // wide moves (alias, lowercase)
+
+    "u": Move(.Uw),
+    "d": Move(.Dw),
+    "f": Move(.Fw),
+    "b": Move(.Bw),
+    "r": Move(.Rw),
+    "l": Move(.Lw),
+
+    "u'": Move(.Uw, prime: true),
+    "d'": Move(.Dw, prime: true),
+    "f'": Move(.Fw, prime: true),
+    "b'": Move(.Bw, prime: true),
+    "r'": Move(.Rw, prime: true),
+    "l'": Move(.Lw, prime: true),
+
+    "u2": Move(.Uw, twice: true),
+    "d2": Move(.Dw, twice: true),
+    "f2": Move(.Fw, twice: true),
+    "b2": Move(.Bw, twice: true),
+    "r2": Move(.Rw, twice: true),
+    "l2": Move(.Lw, twice: true),
+
     // cube rotation
 
     "x": Move(.x),
@@ -122,6 +186,20 @@ let allMoves: [String:Move] = [
     "x2": Move(.x, twice: true),
     "y2": Move(.y, twice: true),
     "z2": Move(.z, twice: true),
+
+    // middle layer
+
+    "M": Move(.M),
+    "E": Move(.E),
+    "S": Move(.S),
+
+    "M'": Move(.M, prime: true),
+    "E'": Move(.E, prime: true),
+    "S'": Move(.S, prime: true),
+
+    "M2": Move(.M, twice: true),
+    "E2": Move(.E, twice: true),
+    "S2": Move(.S, twice: true),
 ]
 
 extension Cube {
