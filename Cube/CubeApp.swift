1269aec0408e0976391805e335f7a9af00c8e5e8
Basuke Suzuki <basuke@mac.com>
2023-08-13 16:40:20 -0700

    Added save and load

--- Cube/CubeApp.swift
+++ Cube/CubeApp.swift
@@ -10,10 +10,27 @@ import SwiftUI
 @main
 struct CubeApp: App {
     @StateObject private var store = DataStore()
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView(cube: $store.cube)
+            .onChange(of: scenePhase) { phase in
+                if phase == .inactive {
+                    do {
+                        try store.save()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+            .task {
+                do {
+                    try store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            }
         }
     }
 }
