9e04f6dc25541de6ec159761515591e86af030c3
Basuke Suzuki <basuke@mac.com>
2023-08-14 21:19:44 -0700

    Added piece node.

--- Cube/Models/Cube3D.swift
+++ Cube/Models/Cube3D.swift
@@ -9,26 +9,44 @@ import Foundation
 import SceneKit
 
 struct Cube3D {
-    let scene: SCNScene
+    let scene = SCNScene()
+    let cubeNode = SCNNode()
+    let pieceNodes: [SCNNode]
 
     init(with cube: Cube) {
-        // Create a scene
-        scene = SCNScene()
-        
-        // Create a box geometry
-        let boxGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
-        
-        // Create a material with red color
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        
-        // Assign the material to the box geometry
-        boxGeometry.materials = [material]
-        
-        // Create a node with the box geometry
-        let boxNode = SCNNode(geometry: boxGeometry)
-        
         // Add the box node to the scene
-        scene.rootNode.addChildNode(boxNode)
+        scene.rootNode.addChildNode(cubeNode)
+
+        func createPiece(_ vec: Vector) -> SCNNode {
+            let base = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0.1)
+            base.firstMaterial?.diffuse.contents = UIColor(white: 0.1, alpha: 0.8)
+
+            let piece = SCNNode(geometry: base)
+
+            piece.position = SCNVector3(vec)
+            return piece
+        }
+
+        // Create each piece
+        let centers: [Float] = [-1, 0, 1]
+        var nodes: [SCNNode] = []
+        for z in centers {
+            for y in centers {
+                for x in centers {
+                    if x != 0 || y != 0 || z != 0 {
+                        let pieceNode = createPiece(Vector(x, y, z))
+                        nodes.append(pieceNode)
+                        cubeNode.addChildNode(pieceNode)
+                    }
+                }
+            }
+        }
+        pieceNodes = nodes
+    }
+}
+
+extension SCNVector3 {
+    init(_ vec: Vector) {
+        self.init(vec.x, vec.y, vec.z)
     }
 }
