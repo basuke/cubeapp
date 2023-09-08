873194bd8492c73ea94b12221f15301708f17465
Basuke Suzuki <basuke@mac.com>
2023-09-07 18:48:58 -0700

    Separate content view for xrOS and others.

--- Cube/Views/ContentView.swift
+++ Cube/Views/ContentView.swift
@@ -14,13 +14,7 @@ struct ContentView: View {
     @ObservedObject var play: Play
     @State private var yawRatio: Float = -1.0
 
-    let gradientColors: [UIColor] = [
-        UIColor.lightGray,
-        UIColor.white,
-        UIColor.lightGray,
-    ]
-
-    #if os(xrOS)
+#if os(xrOS)
     class RealityViewActionRunner: ActionRunner {
         var subscription: EventSubscription? = nil
         var action: Action? = nil
@@ -36,12 +30,10 @@ struct ContentView: View {
             self.action = action
         }
     }
-    #endif
 
     var body: some View {
         VStack {
             ZStack(alignment: .bottom) {
-                #if os(xrOS)
                 RealityView { content in
                     if play.model == nil {
                         let runner = RealityViewActionRunner(content: content)
@@ -54,10 +46,6 @@ struct ContentView: View {
                 } update: { content in
                     play.model?.setCameraYaw(ratio: yawRatio)
                 }
-
-                #else
-                Cube3DView(play: play, yawRatio: $yawRatio)
-                #endif
 
                 HStack {
                     Spacer()
@@ -80,10 +68,44 @@ struct ContentView: View {
                 }
             }
         }
+    }
+#else
+    let gradientColors: [UIColor] = [
+        UIColor.lightGray,
+        UIColor.white,
+        UIColor.lightGray,
+    ]
+
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottom) {
+                Cube3DView(play: play, yawRatio: $yawRatio)
+
+                HStack {
+                    Spacer()
+                    Cube2DView(cube: play.cube.as2D())
+                    Spacer()
+                    Slider(
+                        value: $yawRatio,
+                        in: -3.0...3.0
+                    )
+                        .frame(width: 120)
+                    Spacer()
+                }
+            }
+            MoveController(canUndo: !play.moves.isEmpty) { move in
+                if let move {
+                    play.apply(move: move)
+                } else {
+                    play.undo()
+                }
+            }
+        }
         .background(
             LinearGradient(gradient: Gradient(colors: gradientColors.map { SwiftUI.Color($0) }), startPoint: .top, endPoint: .bottom)
         )
     }
+#endif
 }
 
 #Preview {
