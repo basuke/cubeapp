f0f1a4617655000efb29441eb87715fb5b9b7a8e
Basuke Suzuki <basuke@mac.com>
2023-09-23 09:51:51 -0700

    First test.

--- CubeTests/CubeTests.swift
+++ CubeTests/CubeTests.swift
@@ -6,6 +6,16 @@
 //
 
 import XCTest
+@testable import Cube
 
 final class CubeTests: XCTestCase {
+    func testIdentifyMove() throws {
+        let sticker = Sticker(color: .white, position: Vector(1.5, 1.0, 1.0))
+
+        XCTAssert(sticker.identifyMove(for: .up) == "F'")
+        XCTAssert(sticker.identifyMove(for: .down) == "F")
+
+        XCTAssert(sticker.identifyMove(for: .left) == "U")
+        XCTAssert(sticker.identifyMove(for: .right) == "U'")
+    }
 }
