e61c75fd950364e795e3b8dc483bbc5aa6482a57
Basuke Suzuki <basuke@mac.com>
2023-09-23 11:35:21 -0700

    Complete right face.

--- CubeTests/CubeTests.swift
+++ CubeTests/CubeTests.swift
@@ -12,7 +12,18 @@ final class CubeTests: XCTestCase {
     func testIdentifyMove() throws {
         typealias MoveMap = [Direction:String]
         let tests: [Vector:MoveMap] = [
+            // .right
             Vector(1.5, 1.0, 1.0): [.up: "F'", .down: "F", .left: "U", .right: "U'"],
+            Vector(1.5, 1.0, 0.0): [.up: "S'", .down: "S", .left: "U", .right: "U'"],
+            Vector(1.5, 1.0, -1.0): [.up: "B", .down: "B'", .left: "U", .right: "U'"],
+
+            Vector(1.5, 0.0, 1.0): [.up: "F'", .down: "F", .left: "E'", .right: "E"],
+            Vector(1.5, 0.0, 0.0): [.up: "z'", .down: "z", .left: "y", .right: "y'"],
+            Vector(1.5, 0.0, -1.0): [.up: "B", .down: "B'", .left: "E'", .right: "E"],
+
+            Vector(1.5, -1.0, 1.0): [.up: "F'", .down: "F", .left: "D'", .right: "D"],
+            Vector(1.5, -1.0, 0.0): [.up: "S'", .down: "S", .left: "D'", .right: "D"],
+            Vector(1.5, -1.0, -1.0): [.up: "B", .down: "B'", .left: "D'", .right: "D"],
         ]
 
         for (position, moves) in tests {
