8776f124e1a3c023752bdf4b8646cc4a91910839
Basuke Suzuki <basuke@mac.com>
2023-09-23 11:40:34 -0700

    .up

--- CubeTests/CubeTests.swift
+++ CubeTests/CubeTests.swift
@@ -24,6 +24,19 @@ final class CubeTests: XCTestCase {
             Vector(1.5, -1.0, 1.0): [.up: "F'", .down: "F", .left: "D'", .right: "D"],
             Vector(1.5, -1.0, 0.0): [.up: "S'", .down: "S", .left: "D'", .right: "D"],
             Vector(1.5, -1.0, -1.0): [.up: "B", .down: "B'", .left: "D'", .right: "D"],
+
+            // .up
+            Vector(-1.0, 1.5, 1.0): [.up: "L'", .down: "L", .left: "F'", .right: "F"],
+            Vector(0.0, 1.5, 1.0): [.up: "M'", .down: "M", .left: "F'", .right: "F"],
+            Vector(1.0, 1.5, 1.0): [.up: "R", .down: "R'", .left: "F'", .right: "F"],
+
+            Vector(-1.0, 1.5, 0.0): [.up: "L'", .down: "L", .left: "S'", .right: "S"],
+            Vector(0.0, 1.5, 0.0): [.up: "x", .down: "x'", .left: "z'", .right: "z"],
+            Vector(1.0, 1.5, 0.0): [.up: "R", .down: "R'", .left: "S'", .right: "S"],
+
+            Vector(-1.0, 1.5, -1.0): [.up: "L'", .down: "L", .left: "B", .right: "B'"],
+            Vector(0.0, 1.5, -1.0): [.up: "M'", .down: "M", .left: "B", .right: "B'"],
+            Vector(1.0, 1.5, -1.0): [.up: "R", .down: "R'", .left: "B", .right: "B'"],
         ]
 
         for (position, moves) in tests {
