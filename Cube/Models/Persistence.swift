bd99306b9a33e65b8d51c9f88ee716642845fac9
Basuke Suzuki <basuke@mac.com>
2023-09-30 12:01:22 -0700

    Make constants inside class

--- Cube/Models/Persistence.swift
+++ Cube/Models/Persistence.swift
@@ -7,22 +7,32 @@
 
 import Foundation
 
-let kCubeSaveDataVersion = 1
-let kCubeSaveDataKey = "Cube:\(kCubeSaveDataVersion)"
-
 extension Play {
     struct SaveData: Codable {
+        static let version = 1
+
+        let version: Int
         let cube: Cube
         let moves: [Move]
+
+        init(cube: Cube, moves: [Move]) {
+            self.version = Self.version
+            self.cube = cube
+            self.moves = moves
+        }
+
+        static var key: String {
+            "Cube:\(version)"
+        }
     }
 
     func save() throws {
         let data = SaveData(cube: cube, moves: moves)
-        UserDefaults.standard.setValue(try JSONEncoder().encode(data), forKey: kCubeSaveDataKey)
+        UserDefaults.standard.setValue(try JSONEncoder().encode(data), forKey: SaveData.key)
     }
 
     func load() throws {
-        if let data = UserDefaults.standard.data(forKey: kCubeSaveDataKey) {
+        if let data = UserDefaults.standard.data(forKey: SaveData.key) {
             let saveData = try JSONDecoder().decode(SaveData.self, from: data)
             cube = saveData.cube
             moves = saveData.moves
