77fd06572a8da975f369fb89e4fd4a2ab57dfd25
Basuke Suzuki <basuke@mac.com>
2023-09-30 15:23:39 -0700

    Refactoring

--- CubeTests/StickerTests.swift
+++ CubeTests/StickerTests.swift
@@ -10,130 +10,99 @@ import XCTest
 
 final class StickerTests: XCTestCase {
     func testIdentifyMove() throws {
-        typealias MoveMap = [Direction:String]
-        let tests: [Vector:MoveMap] = [
-            // .right
-            Vector(1.5, 1.0, 1.0): [.up: "F'", .down: "F", .left: "U", .right: "U'"],
-            Vector(1.5, 1.0, 0.0): [.up: "S'", .down: "S", .left: "U", .right: "U'"],
-            Vector(1.5, 1.0, -1.0): [.up: "B", .down: "B'", .left: "U", .right: "U'"],
-
-            Vector(1.5, 0.0, 1.0): [.up: "F'", .down: "F", .left: "E'", .right: "E"],
-            Vector(1.5, 0.0, 0.0): [.up: "z'", .down: "z", .left: "y", .right: "y'"],
-            Vector(1.5, 0.0, -1.0): [.up: "B", .down: "B'", .left: "E'", .right: "E"],
-
-            Vector(1.5, -1.0, 1.0): [.up: "F'", .down: "F", .left: "D'", .right: "D"],
-            Vector(1.5, -1.0, 0.0): [.up: "S'", .down: "S", .left: "D'", .right: "D"],
-            Vector(1.5, -1.0, -1.0): [.up: "B", .down: "B'", .left: "D'", .right: "D"],
-
-            // .up
-            Vector(-1.0, 1.5, 1.0): [.up: "L'", .down: "L", .left: "F'", .right: "F"],
-            Vector(0.0, 1.5, 1.0): [.up: "M'", .down: "M", .left: "F'", .right: "F"],
-            Vector(1.0, 1.5, 1.0): [.up: "R", .down: "R'", .left: "F'", .right: "F"],
-
-            Vector(-1.0, 1.5, 0.0): [.up: "L'", .down: "L", .left: "S'", .right: "S"],
-            Vector(0.0, 1.5, 0.0): [.up: "x", .down: "x'", .left: "z'", .right: "z"],
-            Vector(1.0, 1.5, 0.0): [.up: "R", .down: "R'", .left: "S'", .right: "S"],
-
-            Vector(-1.0, 1.5, -1.0): [.up: "L'", .down: "L", .left: "B", .right: "B'"],
-            Vector(0.0, 1.5, -1.0): [.up: "M'", .down: "M", .left: "B", .right: "B'"],
-            Vector(1.0, 1.5, -1.0): [.up: "R", .down: "R'", .left: "B", .right: "B'"],
-
-            // .front
-            Vector(-1.0, 1.0, 1.5): [.up: "L'", .down: "L", .left: "U", .right: "U'"],
-            Vector(0.0, 1.0, 1.5): [.up: "M'", .down: "M", .left: "U", .right: "U'"],
-            Vector(1.0, 1.0, 1.5): [.up: "R", .down: "R'", .left: "U", .right: "U'"],
-
-            Vector(-1.0, 0.0, 1.5): [.up: "L'", .down: "L", .left: "E'", .right: "E"],
-            Vector(0.0, 0.0, 1.5): [.up: "x", .down: "x'", .left: "y", .right: "y'"],
-            Vector(1.0, 0.0, 1.5): [.up: "R", .down: "R'", .left: "E'", .right: "E"],
-
-            Vector(-1.0, -1.0, 1.5): [.up: "L'", .down: "L", .left: "D'", .right: "D"],
-            Vector(0.0, -1.0, 1.5): [.up: "M'", .down: "M", .left: "D'", .right: "D"],
-            Vector(1.0, -1.0, 1.5): [.up: "R", .down: "R'", .left: "D'", .right: "D"],
-
-            // .left
-            Vector(-1.5, 1.0, -1.0): [.up: "B'", .down: "B", .left: "U", .right: "U'"],
-            Vector(-1.5, 1.0, 0.0): [.up: "S", .down: "S'", .left: "U", .right: "U'"],
-            Vector(-1.5, 1.0, 1.0): [.up: "F", .down: "F'", .left: "U", .right: "U'"],
-
-            Vector(-1.5, 0.0, -1.0): [.up: "B'", .down: "B", .left: "E'", .right: "E"],
-            Vector(-1.5, 0.0, 0.0): [.up: "z", .down: "z'", .left: "y", .right: "y'"],
-            Vector(-1.5, 0.0, 1.0): [.up: "F", .down: "F'", .left: "E'", .right: "E"],
-
-            Vector(-1.5, -1.0, -1.0): [.up: "B'", .down: "B", .left: "D'", .right: "D"],
-            Vector(-1.5, -1.0, 0.0): [.up: "S", .down: "S'", .left: "D'", .right: "D"],
-            Vector(-1.5, -1.0, 1.0): [.up: "F", .down: "F'", .left: "D'", .right: "D"],
+        let tests: [Face:(horizontal: [String], vertical: [String])] = [
+            .right: (horizontal: [
+                "U", "U", "U",
+                "E'","y", "E'",
+                "D'","D'","D'"
+            ], vertical: [
+                "F'","S'","B",
+                "F'","z'","B",
+                "F'","S'","B"
+            ]),
+            .up: (horizontal: [
+                "B", "B", "B",
+                "S'","z'","S'",
+                "F'","F'","F'"
+            ], vertical: [
+                "L'","M'","R",
+                "L'","x", "R",
+                "L'","M'","R"
+            ]),
+            .front: (horizontal: [
+                "U", "U", "U",
+                "E'","y", "E'",
+                "D'","D'","D'"
+            ], vertical: [
+                "L'","M'","R",
+                "L'","x", "R",
+                "L'","M'","R"
+            ]),
+            .left: (horizontal: [
+                "U", "U", "U",
+                "E'","y", "E'",
+                "D'","D'","D'"
+            ], vertical: [
+                "B'","S", "F",
+                "B'","z", "F",
+                "B'","S", "F"
+            ]),
         ]
+        var count = 0
 
-        func test(_ x: Float, _ y: Float, _ z: Float) -> Int {
-            let position = Vector(x, y, z)
-
-            let face: Face = if x == 1.5 {
-                .right
-            } else if y == 1.5 {
-                .up
-            } else if z == 1.5 {
-                .front
-            } else if x == -1.5 {
-                .left
-            } else if y == -1.5 {
-                .down
-            } else if z == -1.5 {
-                .back
-            } else {
-                .right
-            }
-
-            var colors: [Face:Color] = [face:.white]
-            if x == 1.0 || x == -1.0 {
-                colors[.right] = .red
-            }
-            if y == 1.0 || y == -1.0 {
-                colors[.up] = .yellow
-            }
-            if z == 1.0 || z == -1.0 {
-                colors[.front] = .green
-            }
-
-            let piece = Piece(at: (position * 0.9).rounded, colors: colors)
-            let sticker = Sticker(piece: piece, face: face)
+        func test(_ face: Face, _ index: Int) -> Int {
             var count = 0
+            let cube = Cube()
+            let piece = cube.piece(on: face, index: index)
+            let sticker = piece.sticker(on: face)!
 
-            if let moves = tests[position] {
+            if let (horizontalTest, verticalTest) = tests[face] {
+                func parse(_ str: String) -> (String, String) {
+                    let move = str.replacing("'", with: "")
+                    let opposite = move + "'"
+
+                    return str.count == 1 ? (move, opposite) : (opposite, move)
+                }
+
+                let (leftMove, rightMove) = parse(horizontalTest[index])
+                let (upMove, downMove) = parse(verticalTest[index])
+                let moves: [Direction:String] = [.left: leftMove, .right: rightMove, .up: upMove, .down: downMove]
+
                 for (direction, move) in moves {
                     let result = sticker.identifyMove(for: direction)
-                    XCTAssert(result == move, "\(position): \(direction) should be \(move), but \(result ?? "nil")")
+                    XCTAssert(result == move, "\(face)\(index): \(direction) should be \(move), but \(result ?? "nil")")
                     count += 1
                 }
             } else {
                 for direction in Direction.allCases {
                     let result = sticker.identifyMove(for: direction)
-                    XCTAssert(result == nil, "\(position): \(direction) should be nil, but \(result ?? "nil")")
+                    XCTAssert(result == nil, "\(face)\(index): \(direction) should be nil, but \(result ?? "nil")")
                     count += 1
                 }
             }
+
             return count
         }
 
-        let positions: [Float] = [-1, 0, 1]
-        var count = 0
-
-        for z in positions {
-            for y in positions {
-                for x in positions {
-                    if x != 0 {
-                        count += test(x * 1.5, y, z)
-                    }
-                    if y != 0 {
-                        count += test(x, y * 1.5, z)
-                    }
-                    if z != 0 {
-                        count += test(x, y, z * 1.5)
-                    }
-                }
+        for face in Face.allCases {
+            for index in 0..<9 {
+                count += test(face, index)
             }
         }
 
         XCTAssert(count == 3 * 3 * 6 * 4)
+    }
+}
+
+extension Piece {
+    func has(_ face: Face) -> Bool {
+        colors[face] != nil
+    }
+}
+
+extension Cube {
+    func piece(on face: Face, index: Int) -> Piece {
+        let sorted = pieces.filter ({ $0.has(face) }).sorted(by: { $0.index(on: face) < $1.index(on: face) })
+        return sorted[index]
     }
 }
